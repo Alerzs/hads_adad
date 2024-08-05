8b7230463f5b5d859fe6f54f18091556f7ed9da2
Hamidreza <hamidreza.mugen@gmail.com>
2024-08-05 20:49:58 +0330

    Client_guess(Menu)

--- client.py
+++ client.py
@@ -9,3 +9,23 @@ class NumberGuessClient:
    
     def get_client_socket(self):
            return self.__client_socket
+    
+client = NumberGuessClient('localhost', 12345)
+
+
+while True:
+    guess = input("Enter your guess (between 1 and 100): ")
+    if not guess.isdigit():
+        print("Please enter a valid number.")
+        continue
+    
+    guess_number = int(guess)
+
+    client.get_client_socket().send(str(guess_number).encode())
+
+    response = client.get_client_socket().recv(1024).decode()
+    print(response)
+
+    if response == "Correct guess! You got it!":
+        client.get_client_socket().close()
+        break
