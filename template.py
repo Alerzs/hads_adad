cfa0fa8cd611db62400a92bcced07fb2f7ec2ef4
Alireza Shams <alerzs1023@gmail.com>
2024-08-05 11:19:25 +0330

    template

---
+++ template.py
@@ -0,0 +1,42 @@
+class Cliente:
+    
+  def __init__(self) -> None:
+    #connect to server
+    pass
+
+  def guess_number(self):
+    #input and check if it is a number
+    pass
+
+  def send_to_server(self):
+    #send a message to server
+    pass
+
+  def recive_from_server(self):
+    #recive a message from server
+    pass
+
+
+class Server:
+
+  def __init__(self) -> None:
+    #create a server
+    pass
+
+  def rand_number(self):
+    #choose a random number between 1 to 100 and save it as an attribute
+    pass
+
+  def send_to_client(self):
+    #send a message to client
+    pass
+
+  def recive_from_client(self):
+    #recive a message from client
+    pass
+
+  def check_number(self):
+    #checks if the given number is equal to random number
+    pass
+
+  
